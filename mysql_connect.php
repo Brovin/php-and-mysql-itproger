75a1ff23f052a2aa2f6aa6ec07029d9aec7804a2
Boris <fody@protonmail.com>
2019-05-12 00:59:09 +0300

    Added PHP dotenv

--- mysql_connect.php
+++ mysql_connect.php
@@ -1,8 +1,12 @@
 <?php
-  $user = 'root';
-  $password = 'root';
-  $db = 'testing';
-  $host = 'localhost';
+  require_once "vendor/autoload.php";
+  $dotenv = Dotenv\Dotenv::create(__DIR__);
+  $dotenv->load();
+
+  $user = getenv('DB_USERNAME');
+  $password = getenv('DB_PASSWORD');
+  $db = getenv('DB_DATABASE');
+  $host = getenv('DB_HOST');
 
   $dsn = 'mysql:host='.$host.'; dbname='.$db;
   $pdo = new PDO($dsn, $user, $password);
